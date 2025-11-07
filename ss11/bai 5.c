4e3c4a441e514c5dd60811591e8514299c6c6b70
thạch lại lập trình <nguyenthachanhlctu@gmail.com>
2025-11-07 08:19:27 +0700

    Format and clean up code in bai 5.c

--- ss11/bai 5.c
+++ ss11/bai 5.c
@@ -3,25 +3,25 @@
 
 int main() {
     
-    char ds[][20] = {"apple", "banana", "orange", "grape", "mango"};
-    int n = sizeof(ds) / sizeof(ds[0]); 
+        char ds[][20] = {"apple", "banana", "orange", "grape", "mango"};
+        int n = sizeof(ds) / sizeof(ds[0]); 
 
-    char x[20];   
-    int found = 0; 
+            char x[20];   
+            int found = 0; 
 
-    
-    printf("Danh sach cac chuoi trong mang:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", ds[i]);
+                
+            printf("Danh sach cac chuoi trong mang:\n");
+            for (int i = 0; i < n; i++) {
+                    printf("%s\n", ds[i]);
     }
 
    
-    printf("\nNhap chuoi can tim: ");
-    scanf("%s", x);
+            printf("\nNhap chuoi can tim: ");
+            scanf("%s", x);
 
  
-    for (int i = 0; i < n; i++) {
-        if (strcmp(ds[i], x) == 0) { 
+        for (int i = 0; i < n; i++) {
+            if (strcmp(ds[i], x) == 0) { 
             printf("Tim thay chuoi '%s' tai vi tri %d.\n", x, i);
             found = 1;
             break; 
@@ -29,10 +29,11 @@ int main() {
     }
 
   
-    if (!found) {
+        if (!found) {
         printf("Khong tim thay chuoi '%s' trong mang.\n", x);
     }
 
     return 0;
 }
+
 
