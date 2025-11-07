34e2154e7d042334da6b0af53e9490c5b6fa5f88
thạch lại lập trình <nguyenthachanhlctu@gmail.com>
2025-11-07 08:16:48 +0700

    Update and rename bai4.cpp to bai4.c

--- ss11/bai4.c
+++ ss11/bai4.c
@@ -18,7 +18,7 @@ int main() {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (a[j] > a[j + 1]) {
-                // Hoán ð?i v? trí
+                // HoÃ¡n Ã°?i v? trÃ­
                 int temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
